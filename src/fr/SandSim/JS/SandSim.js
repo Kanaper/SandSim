ccf527aaee9d17af1a7f89f60f41dd4198ab2486
orfao <marco.orfao@etu.u-pec.fr>
2024-11-27 21:14:51 +0100

    updated : grid size + scan function

--- src/fr/SandSim/JS/SandSim.js
+++ src/fr/SandSim/JS/SandSim.js
@@ -1,19 +1,71 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-let grid = [600][800];
+const WIDTH = 600;
+const HEIGHT = 900;
+const pixel = WIDTH / 100;
+
+canvas.height = HEIGHT;
+canvas.width = WIDTH;
 
 function drawGrid() {
   ctx.beginPath();
-  for (let i = 1; i <= 100; i++) {
-    ctx.moveTo(i * 6, 0);
-    ctx.lineTo(i * 6, 800);
+  for (let i = 0; i <= WIDTH / pixel; i++) {
+    ctx.moveTo(i * pixel, 0);
+    ctx.lineTo(i * pixel, HEIGHT);
     ctx.stroke();
   }
-  for (let i = 1; i <= 100; i++) {
-    ctx.moveTo(0, i * 8);
-    ctx.lineTo(800, i * 8);
+  for (let i = 0; i <= HEIGHT / pixel; i++) {
+    ctx.moveTo(0, i * pixel);
+    ctx.lineTo(HEIGHT, i * pixel);
     ctx.stroke();
   }
   ctx.closePath();
 }
+
+function makeGrid() {
+  const array = [];
+  for (let i = 0; i < WIDTH / pixel; i++) {
+    const row = [];
+    for (let j = 0; j < HEIGHT / pixel; j++) {
+      row.push(0);
+    }
+    array.push(row);
+  }
+  return array;
+}
+
+function scan() {
+  for (let i = 0; i < WIDTH / pixel; i++) {
+    for (let j = 0; j < HEIGHT / pixel; j++) {
+      if (grid[i][j] == 1) {
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(i * pixel, j * pixel, pixel, 8);
+        if (canFall(grid[i][j + 1])) {
+          grid[i][j + 1] = 1;
+          grid[i][j] = 0;
+        }
+      } else {
+        ctx.fillStyle = "white";
+        ctx.fillRect(i * pixel, j * pixel, pixel, 8);
+      }
+    }
+  }
+}
+
+function canFall(value) {
+  if (value == 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  scan();
+}
+
+const grid = makeGrid();
+grid[50][88] = 1;
+animate();
