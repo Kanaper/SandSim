31ded56c5c7cc2f2696ed2291a4334f31a065895
orfao <marco.orfao@etu.u-pec.fr>
2024-12-02 09:54:53 +0100

    added : doc + Sand ball improvement

--- src/fr/SandSim/JS/SandSim.js
+++ src/fr/SandSim/JS/SandSim.js
@@ -1,40 +1,65 @@
+//initialisation du canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+//largeur et hauteur du canvas
 const WIDTH = 600;
 const HEIGHT = 900;
 
-//modifie la taille du canvas
+//taille du canvas
 canvas.height = HEIGHT;
 canvas.width = WIDTH;
 
+//taille du grain de sable
 const pixel = WIDTH / 200;
+
+//nombre de colonnes et de lignes
 const nrow = HEIGHT / pixel;
 const ncol = WIDTH / pixel;
 
+//initialisation de la variable de la grille théorique
 const grid = makeGrid();
 
+//bouton de la souris pressé
 let mouseDown = false;
+
+//initialisation des coordonnées x et y du curseur de la souris
 let mouseX = 0;
 let mouseY = 0;
+
+//initialisation des variables pour la couleur du sable
 let r = 255,
   g = 255;
 let color;
 
+/**
+ * Mouse Listener du canvas pour détecter si le bouton de la souris est pressée
+ */
 canvas.addEventListener("mousedown", function (event) {
   nextColor();
   mouseDown = true;
 });
 
+/**
+ * Mouse Listener du canvas pour détecter si le bouton de la souris est relachée
+ */
 canvas.addEventListener("mouseup", function (event) {
   mouseDown = false;
 });
 
+/**
+ * Mouse Listener du canvas pour détecter si la souris bouge
+ */
 canvas.addEventListener("mousemove", function (event) {
   mouseX = Math.floor(event.offsetX / pixel);
   mouseY = Math.floor(event.offsetY / pixel);
 });
 
+/**
+ * Fonction pour créer la grille théorique
+ *
+ * @returns une grille de ncol x nrow
+ */
 function makeGrid() {
   const array = [];
   for (let i = 0; i < ncol; i++) {
@@ -47,13 +72,26 @@ function makeGrid() {
   return array;
 }
 
+/**
+ * Fonction qui vérifie si un grain de sable peut tomber
+ *
+ * @param {number} x coordonnée x du grain de sable
+ * @param {number} y coordonnée y du grain de sable
+ * @returns true si le grain de sable peut tomber, false sinon
+ */
 function canFall(x, y) {
   return grid[x][y] == 0;
 }
 
+/**
+ * Fonction qui dessine un grain de sable à une position donnée
+ *
+ * @param {number} x coordonnée x du grain de sable
+ * @param {number} y coordonnée y du grain de sable
+ */
 function drawSand(x, y) {
   if (!isOutOfBound(x, y)) {
-    if (isEmpty(x, y)) {
+    if (canFall(x, y)) {
       ctx.fillStyle = color;
       ctx.fillRect(x * pixel, y * pixel, pixel, pixel);
       grid[x][y] = 1;
@@ -61,13 +99,22 @@ function drawSand(x, y) {
   }
 }
 
+/**
+ * Fonction qui dessine le fond à une position donnée
+ *
+ * @param {number} x coordonnée x du fond
+ * @param {number} y coordonnée y du fond
+ */
 function eraseSand(x, y) {
   ctx.fillStyle = "#000033";
   ctx.fillRect(x * pixel, y * pixel, pixel, pixel);
   grid[x][y] = 0;
 }
 
-function animate(grid) {
+/**
+ * Fonction qui permet d'animer le canvas. Sert de boucle infinie
+ */
+function animate() {
   requestAnimationFrame(animate);
   scan();
   if (mouseDown) {
@@ -75,14 +122,33 @@ function animate(grid) {
   }
 }
 
+/**
+ * Fonction qui dessine plusieurs grains de sable pour former un cercle
+ *
+ * @param {number} x coordonnée x du centre du cercle
+ * @param {number} y coordonnée y du centre du cercle
+ */
 function drawSandBall(x, y) {
+  for (let i = -3; i <= 3; i += 6) {
+    for (let j = -1; j < 2; j++) {
+      drawSand(x + j, y + i);
+    }
+  }
   for (let i = x - 2; i <= x + 2; i++) {
     for (let j = y - 2; j <= y + 2; j++) {
       drawSand(i, j);
     }
   }
+  for (let i = y - 2; i <= y + 2; i++) {
+    for (let j = x - 3; j <= x + 3; j += 6) {
+      drawSand(j, i);
+    }
+  }
 }
 
+/**
+ * Fonction qui change de couleur à chaque appel de la fonction
+ */
 function nextColor() {
   if (g <= 170) {
     r = 255;
@@ -91,14 +157,20 @@ function nextColor() {
   color = `rgb(${(r -= 1)}, ${(g -= 12)}, 0)`;
 }
 
-function isEmpty(x, y) {
-  return grid[x][y] == 0;
-}
-
+/**
+ * Fonction qui vérifie si la souris est en dehors du canvas
+ *
+ * @param {number} x coordonnée x de la souris
+ * @param {number} y coordonnée y de la souris
+ * @returns true si la souris est en dehors du canvas, false sinon
+ */
 function isOutOfBound(x, y) {
   return x < 0 || x >= ncol || y < 0 || y >= nrow;
 }
 
+/**
+ * Fonction qui va scanner la grille de bas en haut pour mettre à jour la position des grains de sable
+ */
 function scan() {
   for (let i = ncol - 1; i >= 0; i--) {
     for (let j = nrow - 1; j >= 0; j--) {
@@ -130,4 +202,5 @@ function scan() {
   }
 }
 
+//lancement de l'animation
 animate(grid);
