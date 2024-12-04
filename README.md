7ac0e2549dd9ab7bbf7977170d6969b55459e699
orfao <marco.orfao@etu.u-pec.fr>
2024-12-04 14:46:08 +0100

    Updated : button style ; Added : Mouse eraser, button listener, flood speed

---
+++ README.md
@@ -0,0 +1,17 @@
+# SandSim
+
+## Description
+
+SandSim est un simulateur de sable dans une boîte.
+
+## Fontionnement
+
+Maintenez le bouton gauche de la souris pour faire apparaître du sable autour du curseur.
+
+## Crédits
+
+- Auteur : Marco Orfao
+
+- Durée approximative de programmation : ~10h30
+
+PS : Ce mini projet a eu pour but premier la découverte du JavaScript
--- src/fr/SandSim/JS/SandSim.js
+++ src/fr/SandSim/JS/SandSim.js
@@ -2,6 +2,9 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+//initialisation du bouton
+const button = document.getElementById("particules");
+
 //largeur et hauteur du canvas
 const WIDTH = 600;
 const HEIGHT = 900;
@@ -11,7 +14,7 @@ canvas.height = HEIGHT;
 canvas.width = WIDTH;
 
 //taille du grain de sable
-const pixel = WIDTH / 200;
+const pixel = WIDTH / 300;
 
 //nombre de colonnes et de lignes
 const nrow = HEIGHT / pixel;
@@ -31,6 +34,12 @@ let mouseY = 0;
 let r = 255,
   g = 255;
 let color;
+
+let floodSpeed = 6;
+
+let eraserSize = 20;
+
+let flood = true;
 
 /**
  * Mouse Listener du canvas pour détecter si le bouton de la souris est pressée
@@ -54,6 +63,50 @@ canvas.addEventListener("mousemove", function (event) {
   mouseX = Math.floor(event.offsetX / pixel);
   mouseY = Math.floor(event.offsetY / pixel);
 });
+
+/**
+ * Mouse Listener du canvas pour détecter si la souris sort du canvas
+ */
+button.addEventListener("click", () => {
+  changeSandFallingMethod();
+  changeButtonBackground();
+});
+
+/**
+ * Fonction qui change la couleur du bouton en fonction de la méthode de chute du sable
+ */
+function changeButtonBackground() {
+  if (!flood) {
+    button.style.backgroundColor = "#4CAF50";
+  } else {
+    button.style.backgroundColor = "#f44336";
+  }
+}
+
+/**
+ * Supprime les grains de sable à la avec un curseur d'une taille eraserSize x eraserSize ayant pour centre la souris
+ *
+ * @param {number} mouseX coordonnée x de la souris
+ * @param {number} mouseY coordonnée y de la souris
+ */
+function mouseEraser(mouseX, mouseY) {
+  for (let i = mouseX - 2; i <= mouseX + eraserSize - 2; i++) {
+    for (let j = mouseY - 2; j <= mouseY + eraserSize - 2; j++) {
+      eraseSand(i, j);
+    }
+  }
+}
+
+/**
+ * Change la méthode de chute du sable
+ */
+function changeSandFallingMethod() {
+  if (flood) {
+    flood = false;
+  } else {
+    flood = true;
+  }
+}
 
 /**
  * Fonction pour créer la grille théorique
@@ -106,19 +159,10 @@ function drawSand(x, y) {
  * @param {number} y coordonnée y du fond
  */
 function eraseSand(x, y) {
-  ctx.fillStyle = "#000033";
-  ctx.fillRect(x * pixel, y * pixel, pixel, pixel);
-  grid[x][y] = 0;
-}
-
-/**
- * Fonction qui permet d'animer le canvas. Sert de boucle infinie
- */
-function animate() {
-  requestAnimationFrame(animate);
-  scan();
-  if (mouseDown) {
-    drawSandBall(mouseX, mouseY);
+  if (!isOutOfBound(x, y)) {
+    ctx.fillStyle = "#000033";
+    ctx.fillRect(x * pixel, y * pixel, pixel, pixel);
+    grid[x][y] = 0;
   }
 }
 
@@ -129,19 +173,9 @@ function animate() {
  * @param {number} y coordonnée y du centre du cercle
  */
 function drawSandBall(x, y) {
-  for (let i = -3; i <= 3; i += 6) {
-    for (let j = -1; j < 2; j++) {
-      drawSand(x + j, y + i);
-    }
-  }
   for (let i = x - 2; i <= x + 2; i++) {
-    for (let j = y - 2; j <= y + 2; j++) {
+    for (let j = y - 2; j <= y + floodSpeed; j++) {
       drawSand(i, j);
-    }
-  }
-  for (let i = y - 2; i <= y + 2; i++) {
-    for (let j = x - 3; j <= x + 3; j += 6) {
-      drawSand(j, i);
     }
   }
 }
@@ -150,11 +184,11 @@ function drawSandBall(x, y) {
  * Fonction qui change de couleur à chaque appel de la fonction
  */
 function nextColor() {
-  if (g <= 170) {
+  if (g <= 150) {
     r = 255;
     g = 255;
   }
-  color = `rgb(${(r -= 1)}, ${(g -= 12)}, 0)`;
+  color = `rgb(${(r -= 1)}, ${(g -= 4)}, 0)`;
 }
 
 /**
@@ -169,36 +203,80 @@ function isOutOfBound(x, y) {
 }
 
 /**
+ * Fonction qui traite le comportement d'un grain de sable à l'instant T
+ *
+ * @param {number} x coordonnée x
+ * @param {number} y coordonnée y
+ */
+function gravity(x, y) {
+  if (!flood) {
+    if (Math.random() < 0.5) {
+      if (x < ncol - 1 && canFall(x + 1, y + 1)) {
+        drawSand(x + 1, y + 1);
+        eraseSand(x, y);
+      } else if (x > 0 && canFall(x - 1, y + 1)) {
+        drawSand(x - 1, y + 1);
+        eraseSand(x, y);
+      }
+    } else {
+      if (x > 0 && canFall(x - 1, y + 1)) {
+        drawSand(x - 1, y + 1);
+        eraseSand(x, y);
+      } else if (x < ncol - 1 && canFall(x + 1, y + 1)) {
+        drawSand(x + 1, y + 1);
+        eraseSand(x, y);
+      }
+    }
+  } else {
+    if (canFall(x, y + floodSpeed)) {
+      drawSand(x, y + floodSpeed);
+      eraseSand(x, y);
+    } else {
+      if (Math.random() < 0.5) {
+        if (x < ncol - 1 && canFall(x + 1, y + 1)) {
+          drawSand(x + 1, y + 1);
+          eraseSand(x, y);
+        } else if (x > 0 && canFall(x - 1, y + 1)) {
+          drawSand(x - 1, y + 1);
+          eraseSand(x, y);
+        }
+      } else {
+        if (x > 0 && canFall(x - 1, y + 1)) {
+          drawSand(x - 1, y + 1);
+          eraseSand(x, y);
+        } else if (x < ncol - 1 && canFall(x + 1, y + 1)) {
+          drawSand(x + 1, y + 1);
+          eraseSand(x, y);
+        }
+      }
+    }
+  }
+}
+
+/**
  * Fonction qui va scanner la grille de bas en haut pour mettre à jour la position des grains de sable
  */
 function scan() {
   for (let i = ncol - 1; i >= 0; i--) {
     for (let j = nrow - 1; j >= 0; j--) {
       if (grid[i][j] == 1) {
-        if (canFall(i, j + 1)) {
-          eraseSand(i, j);
-          drawSand(i, j + 1);
-        } else {
-          if (Math.random() < 0.5) {
-            if (i < ncol - 1 && canFall(i + 1, j + 1)) {
-              eraseSand(i, j);
-              drawSand(i + 1, j + 1);
-            } else if (i > 0 && canFall(i - 1, j + 1)) {
-              eraseSand(i, j);
-              drawSand(i - 1, j + 1);
-            }
-          } else {
-            if (i > 0 && canFall(i - 1, j + 1)) {
-              eraseSand(i, j);
-              drawSand(i - 1, j + 1);
-            } else if (i < ncol - 1 && canFall(i + 1, j + 1)) {
-              eraseSand(i, j);
-              drawSand(i + 1, j + 1);
-            }
-          }
-        }
+        gravity(i, j);
+        nextColor();
       }
     }
+  }
+}
+
+/**
+ * Fonction qui permet d'animer le canvas. Sert de boucle infinie
+ */
+function animate() {
+  requestAnimationFrame(animate);
+  scan();
+  if (mouseDown) {
+    drawSandBall(mouseX, mouseY);
+  } else {
+    mouseEraser(mouseX, mouseY);
   }
 }
 
